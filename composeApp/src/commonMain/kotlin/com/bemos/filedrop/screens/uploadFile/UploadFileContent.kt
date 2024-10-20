c6fb5edf9b6c9e8b67c9ce6607775f528fc45222
bemos <bemo.0@mail.ru>
2024-10-20 15:14:06 +0300

    new screen

---
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/uploadFile/UploadFileContent.kt
@@ -0,0 +1,40 @@
+package com.bemos.filedrop.screens.uploadFile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UploadFileContent(
+    onUploadClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                onUploadClick()
+            }
+        ) {
+            Text(
+                text = "Upload File"
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun UploadFileContentPreview() {
+    UploadFileContent(
+        onUploadClick = {}
+    )
+}
