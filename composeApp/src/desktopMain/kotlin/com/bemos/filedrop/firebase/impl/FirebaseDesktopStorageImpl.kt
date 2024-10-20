03e666d35b3f99677406e8b42e49daf6af48f485
bemos <bemo.0@mail.ru>
2024-10-20 18:30:18 +0300

    > реализация класса для десктопа

---
+++ composeApp/src/desktopMain/kotlin/com/bemos/filedrop/firebase/impl/FirebaseDesktopStorageImpl.kt
@@ -0,0 +1,40 @@
+package com.bemos.filedrop.firebase.impl
+
+import com.bemos.filedrop.firebase.repo.FirebaseDesktopStorageRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class FirebaseDesktopStorageImpl : FirebaseDesktopStorageRepository{
+    override suspend fun fetchFiles(): String {
+        val projectId = "filedropper-37014"
+        val collection = "files"
+        val apiKey = "AIzaSyCDmIntxsjQ-5JCuJZNAxSD2LZ00wHspv4"
+
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    prettyPrint = true
+                })
+            }
+        }
+
+        val url = "https://firestore.googleapis.com/v1/projects/$projectId/databases/(default)/documents/$collection?key=$apiKey"
+
+        return try {
+            val response: HttpResponse = client.get(url)
+            response.bodyAsText()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            "Error"
+        } finally {
+            client.close()
+        }
+    }
+}
