c126851799e53c481ab2485d2ac85dcb3e15639b
bemos <bemo.0@mail.ru>
2024-10-20 18:28:04 +0300

    экран для десктопа, пока что заглушка

---
+++ composeApp/src/desktopMain/kotlin/com/bemos/filedrop/screens/uploadFile/UploadFileScreen.kt
@@ -0,0 +1,13 @@
+package com.bemos.filedrop.screens.uploadFile
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun UploadFileScreen() {
+
+    UploadFileContent {
+
+    }
+
+}
