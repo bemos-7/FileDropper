77e8ce4cb4458fc4a3a1c6fdf9d3492ac8c41ee4
bemos <bemo.0@mail.ru>
2024-10-20 18:27:39 +0300

    viewModel для экрана андроид

---
+++ composeApp/src/androidMain/kotlin/com/bemos/filedrop/screens/uploadFile/vm/UploadFileViewModel.kt
@@ -0,0 +1,10 @@
+package com.bemos.filedrop.screens.uploadFile.vm
+
+import androidx.lifecycle.ViewModel
+import com.bemos.filedrop.firebase.impl.FirebaseAndroidStorageImpl
+
+class UploadFileViewModel(
+    private val firebaseAndroidStorageImpl: FirebaseAndroidStorageImpl
+) : ViewModel() {
+
+}
