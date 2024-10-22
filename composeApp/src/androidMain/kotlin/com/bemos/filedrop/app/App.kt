aee3b28cbb1ba2413f20799496c30cc10896b0ce
bemos <bemo.0@mail.ru>
2024-10-23 00:48:14 +0300

    > add koin module Common

--- composeApp/src/androidMain/kotlin/com/bemos/filedrop/app/App.kt
+++ composeApp/src/androidMain/kotlin/com/bemos/filedrop/app/App.kt
@@ -1,12 +1,19 @@
 package com.bemos.filedrop.app
 
 import android.app.Application
+import com.bemos.filedrop.di.commonModule
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Firebase.initialize(this)
+        startKoin {
+            androidContext(this@App)
+            modules(commonModule)
+        }
     }
 }
