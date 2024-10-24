b4548faec9aef8db0d0cac566b22c1b37a573a03
bemos <bemo.0@mail.ru>
2024-10-24 22:51:28 +0300

    > создал commonModule

---
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/util/theme/Colors.kt
@@ -0,0 +1,11 @@
+package com.bemos.filedrop.screens.util.theme
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+
+    val Black = Color(0xFF262626)
+    val Light_Black = Color(0xFF363636)
+    val White = Color(0xFFd1d1d1)
+
+}
