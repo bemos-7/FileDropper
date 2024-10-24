f58c99251674404361c8e61ea61c17daa3156ea1
bemos <bemo.0@mail.ru>
2024-10-24 22:52:37 +0300

    > добавил размер экрана и передаю desktopModule

--- composeApp/src/desktopMain/kotlin/com/bemos/filedrop/main.kt
+++ composeApp/src/desktopMain/kotlin/com/bemos/filedrop/main.kt
@@ -1,20 +1,27 @@
 package com.bemos.filedrop
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.bemos.filedrop.di.commonModule
+import com.bemos.filedrop.di.desktopModule
 import com.bemos.filedrop.screens.listOfFiles.ListOfFilesScreen
-import com.bemos.filedrop.screens.uploadFile.UploadFileScreen
 import org.koin.core.context.startKoin
 
 fun main() = application {
     startKoin {
-        modules(commonModule)
+        modules(commonModule, desktopModule)
     }
+    val state = rememberWindowState(
+        width = 1100.dp,
+        height = 700.dp
+    )
     Window(
         onCloseRequest = ::exitApplication,
         title = "FileDropper",
         resizable = false,
+        state = state
     ) {
         ListOfFilesScreen()
     }
