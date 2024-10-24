1cd85af7e8c6410b52a4bf799f08b166c5155989
bemos <bemo.0@mail.ru>
2024-10-24 22:55:51 +0300

    > новый экран, включающий список файлов на сервере

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/ListOfFilesContent.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/ListOfFilesContent.kt
@@ -1,25 +1,40 @@
 package com.bemos.filedrop.screens.listOfFiles
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import com.bemos.filedrop.models.File
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.bemos.filedrop.models.Document
 import com.bemos.filedrop.screens.listOfFiles.listItem.FileListItem
+import com.bemos.filedrop.screens.util.theme.Colors.Black
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ListOfFilesContent(
-    filesList: List<File>
+    filesList: List<Document>
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Black),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+            }
             items(
                 items = filesList
             ) {
