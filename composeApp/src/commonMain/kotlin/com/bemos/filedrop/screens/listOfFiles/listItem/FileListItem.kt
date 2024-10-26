d2630adcb8a61e522bd2fcd04d6463c966989c55
bemos <bemo.0@mail.ru>
2024-10-26 21:44:37 +0300

    > поменял кнопку на иконку

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/listItem/FileListItem.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/listItem/FileListItem.kt
@@ -2,42 +2,39 @@ package com.bemos.filedrop.screens.listOfFiles.listItem
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.bemos.filedrop.models.Document
-import com.bemos.filedrop.models.File
 import com.bemos.filedrop.screens.util.theme.Colors.Light_Black
 import com.bemos.filedrop.screens.util.theme.Colors.White
 import kotlinproject.composeapp.generated.resources.Res
-import kotlinproject.composeapp.generated.resources.compose_multiplatform
-import kotlinproject.composeapp.generated.resources.round_insert_drive_file_24
-import kotlinproject.composeapp.generated.resources.round_upload_file_24
+import kotlinproject.composeapp.generated.resources.outline_file_download
+import kotlinproject.composeapp.generated.resources.outline_insert_drive_file
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun FileListItem(
-    file: Document
+    file: Document,
+    onDownloadClick: (String) -> Unit
 ) {
     Card(
         shape = RoundedCornerShape(15.dp),
@@ -56,8 +53,8 @@ fun FileListItem(
             ) {
                 Icon(
                     modifier = Modifier.size(128.dp),
-                    painter = painterResource(Res.drawable.compose_multiplatform),
-                    tint = Color.White,
+                    painter = painterResource(Res.drawable.outline_insert_drive_file),
+                    tint = White,
                     contentDescription = null
                 )
 
@@ -75,15 +72,16 @@ fun FileListItem(
                 horizontalArrangement = Arrangement.End,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(
-                    onClick = {
-
-                    }
-                ) {
-                    Text(
-                        text = "Download"
-                    )
-                }
+                Icon(
+                    modifier = Modifier.size(32.dp)
+                        .clip(RoundedCornerShape(5.dp))
+                        .clickable {
+                        onDownloadClick(file.fields.fileUrl.stringValue)
+                    },
+                    painter = painterResource(Res.drawable.outline_file_download),
+                    tint = White,
+                    contentDescription = null
+                )
 
                 Spacer(modifier = Modifier.width(20.dp))
             }
