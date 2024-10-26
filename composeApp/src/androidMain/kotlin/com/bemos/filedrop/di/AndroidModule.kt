d08eb1bf4422cc6549bca6e23f1399ec35d5a6ff
bemos <bemo.0@mail.ru>
2024-10-26 21:44:21 +0300

    > передаю impl PlatformScreensRepo, и принимаю UploadFileUseCase, FetchFilesUseCase, UploadFileViewModel, ListOfFilesViewModel

--- composeApp/src/androidMain/kotlin/com/bemos/filedrop/di/AndroidModule.kt
+++ composeApp/src/androidMain/kotlin/com/bemos/filedrop/di/AndroidModule.kt
@@ -1,14 +1,31 @@
 package com.bemos.filedrop.di
 
 import android.net.Uri
+import com.bemos.filedrop.di.platform.AndroidPlatformScreenImpl
 import com.bemos.filedrop.di.platform.AndroidPlatformUriImpl
+import com.bemos.filedrop.di.platform.PlatformScreensRepository
 import com.bemos.filedrop.di.platform.PlatformUriRepository
 import com.bemos.filedrop.firebase.impl.FirebaseAndroidStorageImpl
 import com.bemos.filedrop.repository.FirebaseRepository
+import com.bemos.filedrop.screens.listOfFiles.vm.ListOfFilesViewModel
+import com.bemos.filedrop.screens.uploadFile.vm.UploadFileViewModel
+import com.bemos.filedrop.use_cases.FetchFilesUseCase
+import com.bemos.filedrop.use_cases.UploadFileUseCase
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val androidModule = module {
     single<PlatformUriRepository> { (uri: Uri) -> AndroidPlatformUriImpl(uri = uri) }
 
     single<FirebaseRepository> { FirebaseAndroidStorageImpl() }
+
+    single<PlatformScreensRepository> { AndroidPlatformScreenImpl() }
+
+    single { UploadFileUseCase(get()) }
+
+    single { FetchFilesUseCase(get()) }
+
+    viewModel { UploadFileViewModel(get()) }
+
+    viewModel { ListOfFilesViewModel(get()) }
 }
