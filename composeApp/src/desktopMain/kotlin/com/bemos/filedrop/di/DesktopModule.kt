f98e0a78966aef17c2e1140fa12954d29ca1d1e7
bemos <bemo.0@mail.ru>
2024-10-26 21:42:32 +0300

    > передаю impl PlatformScreensRepo

--- composeApp/src/desktopMain/kotlin/com/bemos/filedrop/di/DesktopModule.kt
+++ composeApp/src/desktopMain/kotlin/com/bemos/filedrop/di/DesktopModule.kt
@@ -1,9 +1,12 @@
 package com.bemos.filedrop.di
 
+import com.bemos.filedrop.di.platform.DesktopPlatformScreensImpl
+import com.bemos.filedrop.di.platform.PlatformScreensRepository
 import com.bemos.filedrop.firebase.impl.FirebaseDesktopStorageImpl
 import com.bemos.filedrop.repository.FirebaseRepository
 import org.koin.dsl.module
 
 val desktopModule = module {
     single<FirebaseRepository> { FirebaseDesktopStorageImpl() }
+    single<PlatformScreensRepository> { DesktopPlatformScreensImpl() }
 }
