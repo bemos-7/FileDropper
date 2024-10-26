3fb535989a1a9c44babbed103e565a4d1d92453f
bemos <bemo.0@mail.ru>
2024-10-26 21:46:48 +0300

    > onDownloadClick добавил для скачивания файла

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/ListOfFilesContent.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/ListOfFilesContent.kt
@@ -1,13 +1,17 @@
 package com.bemos.filedrop.screens.listOfFiles
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,11 +20,14 @@ import androidx.compose.ui.unit.dp
 import com.bemos.filedrop.models.Document
 import com.bemos.filedrop.screens.listOfFiles.listItem.FileListItem
 import com.bemos.filedrop.screens.util.theme.Colors.Black
+import com.bemos.filedrop.screens.util.theme.Colors.Light_Black
+import com.bemos.filedrop.screens.util.theme.Colors.White
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ListOfFilesContent(
-    filesList: List<Document>
+    filesList: List<Document>,
+    onDownloadClick: (String) -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -39,9 +46,36 @@ fun ListOfFilesContent(
                 items = filesList
             ) {
                 FileListItem(
-                    file = it
+                    file = it,
+                    onDownloadClick
                 )
             }
         }
     }
+//    Row(
+//        modifier = Modifier
+//            .fillMaxSize(),
+//        verticalAlignment = Alignment.Bottom
+//    ) {
+//        Column {
+//            Spacer(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(1.dp)
+//                    .background(White)
+//            )
+//            Row(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(15.dp)
+//                    .background(Light_Black),
+//                horizontalArrangement = Arrangement.End
+//            ) {
+//                Text(
+//                    text = "v1.0.0  ",
+//                    color = White
+//                )
+//            }
+//        }
+//    }
 }
