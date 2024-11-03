b7a8cd760de0cbcb76d1a19e62a03eaa616830ee
bemos <bemo.0@mail.ru>
2024-11-03 21:50:48 +0300

    > Добавил onComplete в функцию

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/repository/FirebaseRepository.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/repository/FirebaseRepository.kt
@@ -1,11 +1,12 @@
 package com.bemos.filedrop.repository
 
 import com.bemos.filedrop.di.platform.PlatformUriRepository
+import com.bemos.filedrop.models.DocumentAndroid
 
 interface FirebaseRepository {
 
     fun uploadFile(fileUri: PlatformUriRepository, fileName: String, onComplete: () -> Unit)
 
-    suspend fun fetchFiles(): String
+    suspend fun fetchFiles(onComplete: (List<DocumentAndroid>) -> Unit): String
 
 }
