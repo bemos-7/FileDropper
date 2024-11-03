b71560379a3ea95b6d832c3d975bea7963962b83
bemos <bemo.0@mail.ru>
2024-11-03 21:50:08 +0300

    > Изменил название

--- composeApp/src/androidMain/kotlin/com/bemos/filedrop/di/AndroidModule.kt
+++ composeApp/src/androidMain/kotlin/com/bemos/filedrop/di/AndroidModule.kt
@@ -5,7 +5,7 @@ import com.bemos.filedrop.di.platform.AndroidPlatformScreenImpl
 import com.bemos.filedrop.di.platform.AndroidPlatformUriImpl
 import com.bemos.filedrop.di.platform.PlatformScreensRepository
 import com.bemos.filedrop.di.platform.PlatformUriRepository
-import com.bemos.filedrop.firebase.impl.FirebaseAndroidStorageImpl
+import com.bemos.filedrop.firebase.impl.AndroidFirebaseImpl
 import com.bemos.filedrop.repository.FirebaseRepository
 import com.bemos.filedrop.screens.listOfFiles.vm.ListOfFilesViewModel
 import com.bemos.filedrop.screens.uploadFile.vm.UploadFileViewModel
@@ -17,7 +17,7 @@ import org.koin.dsl.module
 val androidModule = module {
     single<PlatformUriRepository> { (uri: Uri) -> AndroidPlatformUriImpl(uri = uri) }
 
-    single<FirebaseRepository> { FirebaseAndroidStorageImpl() }
+    single<FirebaseRepository> { AndroidFirebaseImpl() }
 
     single<PlatformScreensRepository> { AndroidPlatformScreenImpl() }
 
