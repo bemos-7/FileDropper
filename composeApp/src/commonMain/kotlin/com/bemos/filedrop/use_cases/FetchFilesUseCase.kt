35ea975d7cd55750f793f429e83d140b8404f9ca
bemos <bemo.0@mail.ru>
2024-11-03 21:50:59 +0300

    > Добавил onComplete

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/use_cases/FetchFilesUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/use_cases/FetchFilesUseCase.kt
@@ -1,12 +1,14 @@
 package com.bemos.filedrop.use_cases
 
 import com.bemos.filedrop.di.platform.PlatformUriRepository
+import com.bemos.filedrop.models.Document
+import com.bemos.filedrop.models.DocumentAndroid
 import com.bemos.filedrop.repository.FirebaseRepository
 
 class FetchFilesUseCase(
     private val repository: FirebaseRepository
 ) {
-    suspend fun execute() : String {
-        return repository.fetchFiles()
+    suspend fun execute(onComplete: (List<DocumentAndroid>) -> Unit) : String {
+        return repository.fetchFiles(onComplete)
     }
 }
