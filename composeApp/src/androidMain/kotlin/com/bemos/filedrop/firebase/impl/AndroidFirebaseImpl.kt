c03ea653819780004a570e625129b9bb5d3f278d
bemos <bemo.0@mail.ru>
2024-11-07 22:42:19 +0300

    > поменля модель данных

--- composeApp/src/androidMain/kotlin/com/bemos/filedrop/firebase/impl/AndroidFirebaseImpl.kt
+++ composeApp/src/androidMain/kotlin/com/bemos/filedrop/firebase/impl/AndroidFirebaseImpl.kt
@@ -4,10 +4,8 @@ import android.util.Log
 import androidx.core.net.toUri
 import com.bemos.filedrop.di.platform.PlatformUriRepository
 import com.bemos.filedrop.models.Document
-import com.bemos.filedrop.models.DocumentAndroid
 import com.bemos.filedrop.models.File
 import com.bemos.filedrop.repository.FirebaseRepository
-import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObject
 import com.google.firebase.storage.FirebaseStorage
@@ -48,12 +46,12 @@ class AndroidFirebaseImpl : FirebaseRepository {
             }
     }
 
-    override suspend fun fetchFiles(onComplete: (List<DocumentAndroid>) -> Unit): String {
+    override suspend fun fetchFiles(onComplete: (List<Document>) -> Unit): String {
         val firestore = FirebaseFirestore.getInstance()
         val fileRef = firestore.collection("files")
         fileRef.get()
             .addOnSuccessListener { documents ->
-                val uniqueModels = documents.map { it.toObject<DocumentAndroid>() }
+                val uniqueModels = documents.map { it.toObject<Document>() }
                 Log.d("FirebaseFetchFiles", uniqueModels.toString())
                 onComplete(uniqueModels)
             }
