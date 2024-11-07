3b6f7cd36462e61eb8c78772f0c3efa613a89afd
bemos <bemo.0@mail.ru>
2024-11-07 22:39:39 +0300

    > убрал зависимости

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/ListOfFilesContent.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/listOfFiles/ListOfFilesContent.kt
@@ -1,28 +1,20 @@
 package com.bemos.filedrop.screens.listOfFiles
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.bemos.filedrop.models.Document
 import com.bemos.filedrop.screens.listOfFiles.listItem.FileListItem
 import com.bemos.filedrop.screens.util.theme.Colors.Black
-import com.bemos.filedrop.screens.util.theme.Colors.Light_Black
-import com.bemos.filedrop.screens.util.theme.Colors.White
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ListOfFilesContent(
