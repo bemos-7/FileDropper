4b595c9575ca5b2047106109aa62dae143505988
bemos <bemo.0@mail.ru>
2024-11-07 22:40:27 +0300

    > видоизменил экран полностью

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/uploadFile/UploadFileContent.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/uploadFile/UploadFileContent.kt
@@ -1,32 +1,72 @@
 package com.bemos.filedrop.screens.uploadFile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.bemos.filedrop.screens.util.theme.Colors
+import com.bemos.filedrop.screens.util.theme.buttons.FButton
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.outline_upload_file_24
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun UploadFileContent(
-    onUploadClick: () -> Unit
+    onUploadClick: () -> Unit,
+    onFilesClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Colors.Black),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
+        FButton(
+            modifier = Modifier,
             onClick = {
                 onUploadClick()
+            },
+            content = {
+                Icon(
+                    modifier = Modifier.size(200.dp),
+                    painter = painterResource(
+                        resource = Res.drawable.outline_upload_file_24
+                    ),
+                    contentDescription = null,
+                    tint = Colors.White
+                )
             }
-        ) {
-            Text(
-                text = "Upload File"
-            )
-        }
+        )
+
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.End
+    ) {
+        FButton(
+            modifier = Modifier.padding(10.dp),
+            onClick = {
+                onFilesClick()
+            },
+            content = {
+                Text(
+                    text = "files",
+                    color = Colors.White
+                )
+            }
+        )
     }
 }
