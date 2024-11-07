849f5b089c0dc8f7b939af63bb308c594276a278
bemos <bemo.0@mail.ru>
2024-11-07 22:41:58 +0300

    > новая не реализованная функция

--- composeApp/src/desktopMain/kotlin/com/bemos/filedrop/di/platform/DesktopPlatformScreensImpl.kt
+++ composeApp/src/desktopMain/kotlin/com/bemos/filedrop/di/platform/DesktopPlatformScreensImpl.kt
@@ -10,6 +10,9 @@ class DesktopPlatformScreensImpl() : PlatformScreensRepository {
         UploadFileContent(
             onUploadClick = {
 
+            },
+            onFilesClick = {
+
             }
         )
     }
