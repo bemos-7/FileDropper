0f0200aa13051182c42576bac058e66ea654d935
bemos <bemo.0@mail.ru>
2024-11-07 22:41:02 +0300

    > поменял модель данных

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/use_cases/FetchFilesUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/use_cases/FetchFilesUseCase.kt
@@ -1,14 +1,12 @@
 package com.bemos.filedrop.use_cases
 
-import com.bemos.filedrop.di.platform.PlatformUriRepository
 import com.bemos.filedrop.models.Document
-import com.bemos.filedrop.models.DocumentAndroid
 import com.bemos.filedrop.repository.FirebaseRepository
 
 class FetchFilesUseCase(
     private val repository: FirebaseRepository
 ) {
-    suspend fun execute(onComplete: (List<DocumentAndroid>) -> Unit) : String {
+    suspend fun execute(onComplete: (List<Document>) -> Unit) : String {
         return repository.fetchFiles(onComplete)
     }
 }
