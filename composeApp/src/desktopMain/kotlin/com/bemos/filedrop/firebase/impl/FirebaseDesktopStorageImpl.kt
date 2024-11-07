0615f00ffc18b5486a647f65d7ff1c57af2744d0
bemos <bemo.0@mail.ru>
2024-11-07 22:40:42 +0300

    > поменял список

--- composeApp/src/desktopMain/kotlin/com/bemos/filedrop/firebase/impl/FirebaseDesktopStorageImpl.kt
+++ composeApp/src/desktopMain/kotlin/com/bemos/filedrop/firebase/impl/FirebaseDesktopStorageImpl.kt
@@ -1,7 +1,7 @@
 package com.bemos.filedrop.firebase.impl
 
 import com.bemos.filedrop.di.platform.PlatformUriRepository
-import com.bemos.filedrop.models.DocumentAndroid
+import com.bemos.filedrop.models.Document
 import com.bemos.filedrop.repository.FirebaseRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
@@ -17,7 +17,7 @@ class FirebaseDesktopStorageImpl : FirebaseRepository{
 
     }
 
-    override suspend fun fetchFiles(onComplete: (List<DocumentAndroid>) -> Unit): String {
+    override suspend fun fetchFiles(onComplete: (List<Document>) -> Unit): String {
         val projectId = "filedropper-37014"
         val collection = "files"
         val apiKey = "AIzaSyCDmIntxsjQ-5JCuJZNAxSD2LZ00wHspv4"
