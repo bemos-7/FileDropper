8b96be8ae8421a85b7e173ef2a6a41ca188eb782
bemos <bemo.0@mail.ru>
2024-11-08 17:11:26 +0300

    > добавил navController, добавил новый FilePicker, сделал переход

--- composeApp/src/desktopMain/kotlin/com/bemos/filedrop/di/platform/DesktopPlatformScreensImpl.kt
+++ composeApp/src/desktopMain/kotlin/com/bemos/filedrop/di/platform/DesktopPlatformScreensImpl.kt
@@ -1,18 +1,32 @@
 package com.bemos.filedrop.di.platform
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavController
 import com.bemos.filedrop.screens.uploadFile.UploadFileContent
+import com.darkrockstudios.libraries.mpfilepicker.DirectoryPicker
+import com.darkrockstudios.libraries.mpfilepicker.FilePicker
 
-class DesktopPlatformScreensImpl() : PlatformScreensRepository {
+class DesktopPlatformScreensImpl : PlatformScreensRepository {
 
     @Composable
-    override fun UploadFileScreen() {
+    override fun UploadFileScreen(navController: NavController) {
+        var showFilePicker by remember {
+            mutableStateOf(false)
+        }
+        FilePicker(show = showFilePicker) { platformFile ->
+            showFilePicker = false
+            println(platformFile)
+        }
         UploadFileContent(
             onUploadClick = {
-
+                showFilePicker = true
             },
             onFilesClick = {
-
+                navController.navigate("listOfFiles")
             }
         )
     }
