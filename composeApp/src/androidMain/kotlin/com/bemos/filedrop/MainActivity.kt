83ba859ca7eedb94f4a8c300f81649cd6da59ba0
bemos <bemo.0@mail.ru>
2024-11-08 17:15:08 +0300

    > создаю navController и передаю его в AppUi, и также реализовал intent в по ссылке

--- composeApp/src/androidMain/kotlin/com/bemos/filedrop/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/bemos/filedrop/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.bemos.filedrop
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
 import com.bemos.filedrop.di.platform.AndroidPlatformScreenImpl
 import com.bemos.filedrop.screens.uploadFile.UploadFileScreen
 
@@ -10,11 +13,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
+            val navController = rememberNavController()
             AppUi(
+                navController = navController,
                 uploadFileScreen = AndroidPlatformScreenImpl(),
-                onDownloadClick = {}
+                onDownloadClick = { stringUrl ->
+                    startActivity(
+                        Intent(Intent.ACTION_VIEW, Uri.parse(stringUrl))
+                    )
+                }
             )
         }
     }
