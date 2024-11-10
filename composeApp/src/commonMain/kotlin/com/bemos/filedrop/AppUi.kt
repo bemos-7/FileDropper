6fbef37099b3582e89167601c745a2a8bd082aba
bemos <bemo.0@mail.ru>
2024-11-10 22:55:59 +0300

    > поменял на константы

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/AppUi.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/AppUi.kt
@@ -12,6 +12,8 @@ import androidx.navigation.compose.rememberNavController
 import com.bemos.filedrop.di.platform.PlatformScreensRepository
 import com.bemos.filedrop.screens.listOfFiles.ListOfFilesScreen
 import com.bemos.filedrop.screens.uploadFile.UploadFileScreen
+import com.bemos.filedrop.screens.util.Constants.LIST_OF_FILES
+import com.bemos.filedrop.screens.util.Constants.UPLOAD_FILE
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -26,7 +28,7 @@ fun AppUi(
         NavHost(
             modifier = modifier,
             navController = navController,
-            startDestination = "uploadFile"
+            startDestination = UPLOAD_FILE
         ) {
             uploadFile(
                 navController,
@@ -44,7 +46,7 @@ private fun NavGraphBuilder.uploadFile(
     uploadFileScreen: PlatformScreensRepository
 ) {
     composable(
-        route = "uploadFile"
+        route = UPLOAD_FILE
     ) {
         UploadFileScreen(
             navController = navController,
@@ -57,7 +59,7 @@ private fun NavGraphBuilder.listOfFiles(
     onDownloadClick: (String) -> Unit
 ) {
     composable(
-        route = "listOfFiles"
+        route = LIST_OF_FILES
     ) {
         ListOfFilesScreen(
             onDownloadClick =  onDownloadClick
