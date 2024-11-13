8339ac5e21a59d01391044554869785fa5f4f01e
bemos <bemo.0@mail.ru>
2024-11-13 22:21:48 +0300

    > добавил константы

--- composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/util/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/bemos/filedrop/screens/util/Constants.kt
@@ -2,7 +2,14 @@ package com.bemos.filedrop.screens.util
 
 object Constants {
 
+    const val PROJECT_ID = "filedropper-37014"
+    const val COLLECTION = "files"
+    const val API_KEY = "AIzaSyCDmIntxsjQ-5JCuJZNAxSD2LZ00wHspv4"
+
     const val UPLOAD_FILE = "uploadFile"
     const val LIST_OF_FILES = "listOfFiles"
+
+    const val FILES = "files"
+    const val UPLOADS = "uploads/"
 
 }
